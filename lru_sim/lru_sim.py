2429f39b04a1817607d16e0f8d75aa5df79b4ccc
jimin1203 <janny1203@naver.com>
2024-03-21 23:56:53 +0900

    Complete do_sim

--- lru_sim/lru_sim.py
+++ lru_sim/lru_sim.py
@@ -1,22 +1,29 @@
-
-
 class CacheSimulator:
     def __init__(self, cache_slots):
-        self.cache_slots = cache_slots
+        self.cache_size = cache_slots  
+        self.cache_slots = []  
         self.cache_hit = 0
         self.tot_cnt = 1
+
     
     def do_sim(self, page):
-        pass
-        # Do programming here! 
-        
+        if page in self.cache_slots:
+            self.cache_slots.remove(page)
+            self.cache_slots.append(page)
+            self.cache_hit+=1
+        else:
+            if len(self.cache_slots) >= self.cache_size:
+                self.cache_slots.pop(0)
+            self.cache_slots.append(page)
+        self.tot_cnt+=1
+
     def print_stats(self):
-        print("cache_slot = ", self.cache_slots, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
+        print("cache_slot = ", self.cache_size, "cache_hit = ", self.cache_hit, "hit ratio = ", round(self.cache_hit / self.tot_cnt, 4))
 
 
 if __name__ == "__main__":
 
-    data_file = open("./linkbench.trc")
+    data_file = open("lru_sim/linkbench.trc")
     lines = data_file.readlines()
     for cache_slots in range(100, 1001, 100):
         cache_sim = CacheSimulator(cache_slots)
@@ -25,4 +32,3 @@ if __name__ == "__main__":
             cache_sim.do_sim(page)
         
         cache_sim.print_stats()
-
